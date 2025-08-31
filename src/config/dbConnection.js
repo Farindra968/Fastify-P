9d08349493cac711de4ddcef1e2d9b63541a74a7
Farindra Bahadur Bhandari <127407525+Farindra968@users.noreply.github.com>
2025-08-31 20:29:13 +0545

    Update dbConnection.js

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -3,7 +3,7 @@ import { InitUser } from "../Model/User.Model.js";
 import pg from "pg"
 
 const sequelize = new Sequelize(
-  "postgresql://neondb_owner:npg_ksDwVMgXdi89@ep-super-base-a16o1a8d-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+  "postgresql://xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxe",
   {
     dialect: "postgres",
     dialectModule:pg
